4995525624cd0ac022fa4e5da4a1a3887e5bc48b
Saddiqq <9adi8.tm@gmail.com>
2023-06-16 20:24:25 +0400

    added the RecipeResponce to the RecipeService.java Class content

--- Recipe-Management-API/src/main/java/com/example/Recipe/Management/API/Repository/RecipeRepository.java
+++ Recipe-Management-API/src/main/java/com/example/Recipe/Management/API/Repository/RecipeRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 // The RecipeRepository interface extends the JpaRepository interface
 // The JpaRepository provides CRUD (Create, Read, Update, Delete) operations for the Recipe entity
-public interface RecipeRepository extends JpaRepository<Recipe, Long>{
+public interface RecipeRepository extends JpaRepository<Recipe, Integer>{
 
 
 
--- Recipe-Management-API/src/main/java/com/example/Recipe/Management/API/Service/RecipeService.java
+++ Recipe-Management-API/src/main/java/com/example/Recipe/Management/API/Service/RecipeService.java
@@ -3,6 +3,7 @@ package com.example.Recipe.Management.API.Service;
 import com.example.Recipe.Management.API.Model.Recipe;
 import com.example.Recipe.Management.API.Repository.RecipeRepository;
 import com.example.Recipe.Management.API.RequestObject.RecipeRequest;
+import com.example.Recipe.Management.API.ResponseObject.RecipeResponce;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,4 +24,14 @@ public class RecipeService {
         recipeRepository.save(recipe);
         return "Success";
     }
+
+    public RecipeResponce getRecipeById(Integer recipeId) {
+        return RecipeResponce.convertToResponse(recipeRepository.findById(recipeId).get());
+/*/    Find the recipe in the database by its ID using the recipeRepository,
+       Convert it to a RecipeResponse object, and return it.
+ */
+    }
 }
+
+
+
