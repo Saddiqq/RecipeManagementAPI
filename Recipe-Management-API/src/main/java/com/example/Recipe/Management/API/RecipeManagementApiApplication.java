2e155fd5c08ec97e9ce6eb9dedc067caf451475c
Saddiqq <9adi8.tm@gmail.com>
2023-06-16 15:10:18 +0400

    Setting up

---
+++ Recipe-Management-API/src/main/java/com/example/Recipe/Management/API/RecipeManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Recipe.Management.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeManagementApiApplication.class, args);
+	}
+
+}
---
+++ Recipe-Management-API/src/test/java/com/example/Recipe/Management/API/RecipeManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Recipe.Management.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
