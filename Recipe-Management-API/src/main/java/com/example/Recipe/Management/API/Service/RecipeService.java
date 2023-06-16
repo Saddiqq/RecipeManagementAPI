cc78674a79e73babe61f02ea2b3776ce4c09ce97
Saddiqq <9adi8.tm@gmail.com>
2023-06-16 19:51:12 +0400

    added the RecipeService.java Class content

--- Recipe-Management-API/src/main/java/com/example/Recipe/Management/API/Service/RecipeService.java
+++ Recipe-Management-API/src/main/java/com/example/Recipe/Management/API/Service/RecipeService.java
@@ -1,4 +1,26 @@
 package com.example.Recipe.Management.API.Service;
 
+import com.example.Recipe.Management.API.Model.Recipe;
+import com.example.Recipe.Management.API.Repository.RecipeRepository;
+import com.example.Recipe.Management.API.RequestObject.RecipeRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
 public class RecipeService {
+
+    @Autowired //Autowired to interact with the database.
+
+    RecipeRepository recipeRepository;
+
+    public String saveRecipe(RecipeRequest recipeRequest) {
+        Recipe recipe = new Recipe();
+        recipe = RecipeRequest.convertToEntity(recipe, recipeRequest);
+        recipe.setCreatedDate(new Date());
+        recipe.setIsActive(Boolean.TRUE);
+        recipeRepository.save(recipe);
+        return "Success";
+    }
 }
