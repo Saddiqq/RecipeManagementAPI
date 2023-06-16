cadbf275617843a25b48c91c97aaa3c5231a80ac
Saddiqq <9adi8.tm@gmail.com>
2023-06-16 18:56:14 +0400

    Added SQL Connection

--- Recipe-Management-API/src/main/java/com/example/Recipe/Management/API/Repository/RecipeRepository.java
+++ Recipe-Management-API/src/main/java/com/example/Recipe/Management/API/Repository/RecipeRepository.java
@@ -1,4 +1,18 @@
 package com.example.Recipe.Management.API.Repository;
 
-public interface RecipeRepository {
+import com.example.Recipe.Management.API.Model.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // Indicates that this interface is a repository that handles data access
+
+// The RecipeRepository interface extends the JpaRepository interface
+// The JpaRepository provides CRUD (Create, Read, Update, Delete) operations for the Recipe entity
+public interface RecipeRepository extends JpaRepository<Recipe, Long>{
+
+
+
+
+    // The RecipeRepository interface is parameterized with the Recipe entity type and the type of the primary key (Long in this case)
 }
+
