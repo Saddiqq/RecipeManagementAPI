95afc73f46ccf9f756521b26848506566f7ed63e
Saddiqq <9adi8.tm@gmail.com>
2023-06-16 19:39:40 +0400

    added the RecipeRequest.java Class content

--- Recipe-Management-API/src/main/java/com/example/Recipe/Management/API/RequestObject/RecipeRequest.java
+++ Recipe-Management-API/src/main/java/com/example/Recipe/Management/API/RequestObject/RecipeRequest.java
@@ -1,4 +1,23 @@
 package com.example.Recipe.Management.API.RequestObject;
 
+import com.example.Recipe.Management.API.Model.Recipe;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class RecipeRequest {
+    String recipeName;
+    List<String> recipeIngredients;
+    String recipeInstructions;
+    Integer recipeCookingTime;
+
+
+    public static Recipe convertToEntity(Recipe entity, RecipeRequest request){
+        entity.setCookingTime(request.getRecipeCookingTime());
+        entity.setName(request.getRecipeName());
+        entity.setInstructions(request.getRecipeInstructions());
+        entity.setIngredients(request.getRecipeIngredients());
+        return entity;
+    }
 }
