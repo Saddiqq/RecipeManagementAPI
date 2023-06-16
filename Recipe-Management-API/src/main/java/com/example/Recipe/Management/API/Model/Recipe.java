7e070259e2d0f424e2f8b8228358aee6f4fcc138
Saddiqq <9adi8.tm@gmail.com>
2023-06-16 16:06:43 +0400

    added the Recipe.java Class content w/ the required documentation

--- Recipe-Management-API/src/main/java/com/example/Recipe/Management/API/Model/Recipe.java
+++ Recipe-Management-API/src/main/java/com/example/Recipe/Management/API/Model/Recipe.java
@@ -1,4 +1,25 @@
 package com.example.Recipe.Management.API.Model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Recipe {
+@Getter
+@Setter
+@Entity
+@Data
+
+public class Recipe extends  BaseEntity{
+    // Specifies that the `id` field is the primary key and will be automatically generated
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+
+    long id;
+String name; //name of the recipe
+String ingredients; //list of ingredients required for the recipe
+String instruction; //instructions for preparing the recipe
+Integer CookingTime; //cooking time required
 }
